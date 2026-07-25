cc00121a9893cf8972b795974ae1d381a8d056b6
Squishalo <squiishalo@gmail.com>
2026-07-25 13:24:51 +0100

    Cambia el link de pago de Stripe a SumUp

--- src/app/LandingClient.tsx
+++ src/app/LandingClient.tsx
@@ -311,7 +311,7 @@ export default function LandingClient() {
               {t.pricing.warning}
             </p>
             <a
-              href="https://buy.stripe.com/6oU7sL31549S0AC0jv2Ry0m"
+              href="https://pay.sumup.com/b2c/XKZ3A2B5PS"
               className="inline-block bg-[#a9812f] text-white font-bold px-10 py-3.5 rounded-md hover:bg-[#8a691f] transition w-full sm:w-auto"
             >
               {t.pricing.cta}
